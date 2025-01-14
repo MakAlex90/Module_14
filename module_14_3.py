fa854b1cbc76c8d60e63d6a41524a45cfcaf267b
MakAlex90 <meskalin4ik9@gmail.com>
2025-01-14 15:14:27 +0300

    Update module_14_3.py

--- module_14_3.py
+++ module_14_3.py
@@ -4,7 +4,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-api = "7786861788:AAFFn2DS9AASUyduh4mRFMZNgA25R4s-jn4"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
